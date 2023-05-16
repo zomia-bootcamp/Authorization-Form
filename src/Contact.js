0c06a08ab0357aa8bb1f9beb3cc4c4d3f8137b41
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-16 13:21:11 +0300

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Contact() {
+  const password = 'swordfish';
+  const [authorized, setAuthorized] = useState(false);
+
+  function handleSubmit(e) {
+    const enteredPassword = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = enteredPassword == password;
+    setAuthorized(auth)
+  }
+
+  return (
+      <div id="authorization">
+        <h1>Contact</h1>
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
+      </div>
+  );
+}
+
+export default Contact;
